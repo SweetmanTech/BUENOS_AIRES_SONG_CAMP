bf8a15eecbb03cd00191e71ee6160753668c9495
Sweets Sweetman <sweetmantech@gmail.com>
2024-02-10 17:40:26 -0300

    I play videos in a random order.

--- lib/getDefaultFeed.tsx
+++ lib/getDefaultFeed.tsx
@@ -1,52 +1,28 @@
+import { behindTheCamp } from "./behindTheCamp"
+
 const getDefaultFeed = async () => {
-  const tokens = [
-    {
-      name: "sol siete",
-      tokenId: 2,
-      description: "",
-      image: "",
-      animation_url: "",
-      content: {
-        mime: "video/mp4",
-        uri: "",
-      },
+  const first = {
+    name: "sol siete",
+    tokenId: 2,
+    description: "",
+    image: "",
+    animation_url: "",
+    content: {
+      mime: "video/mp4",
+      uri: "",
     },
-    {
-      name: "sol siete",
-      tokenId: 2,
-      description: "",
-      image: "/images/sol.png",
-      animation_url: "/videos/sol.mov",
-      content: {
-        mime: "video/mp4",
-        uri: "/videos/sol.mov",
-      },
-    },
-    {
-      name: "paula pazos",
-      tokenId: 3,
-      description: "",
-      image: "/images/paula.png",
-      animation_url: "/videos/paula.mp4",
-      content: {
-        mime: "video/mp4",
-        uri: "/videos/paula.mp4",
-      },
-      attributes: [],
-    },
-    {
-      name: "0xgonzalo",
-      tokenId: 4,
-      description: "",
-      image: "/images/0xgonzalo.png",
-      animation_url: "/videos/0xgonzalo.mp4",
-      content: {
-        mime: "video/mp4",
-        uri: "/videos/0xgonzalo.mp4",
-      },
-    },
-  ]
-  return tokens
+  }
+
+  // Create a copy of the behindTheCamp array and shuffle it
+  const shuffledBehindTheCamp = [...behindTheCamp].sort(() => 0.5 - Math.random()) // Shuffle the copied elements
+
+  // Unshift the first item to the shuffled copy
+  shuffledBehindTheCamp.unshift(first)
+
+  console.log("SWEETS behindTheCamp", behindTheCamp)
+  console.log("SWEETS SHUFFLED", shuffledBehindTheCamp)
+
+  return shuffledBehindTheCamp
 }
 
 export default getDefaultFeed
